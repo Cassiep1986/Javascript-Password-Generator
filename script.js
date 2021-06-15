208e8dd5002bf5a972de6660ba66618edb509150
seacrest <cassiep1986@gmail.com>
2021-06-15 16:05:18 -0700

    adding function to choices

--- script.js
+++ script.js
@@ -1,41 +1,53 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
+var passwordResult = [];
+var uppercase = "QWERTYUIOPASDFHGHJKLZXCVBNM"
+var lowercase = "qwertyuiopasdfghjklzxcvbnm"
+var numbers = "0123456789"
+var Special ="!@#$%^&*?"
 // Start series of prompts
 function generatePassword(){
   var intro = window.confirm("To generate your password, please answers the following questions.");
   if (!intro) {
     return;  
-  } else { choice1();
+} else { 
+  choice1();
   }}
-
-//Asks for length of password
+  
+  //Asks for length of password
   function choice1() {
     var charlength = window.prompt("1 of 5: How many characters do you want your password to be? (Note: Number must be between 8-128)");
-    if (charlength < 8 || charlength > 128) {window.prompt("Please start over and select a number between 8-128");
-  } else { choice2()  
+    if (charlength < 8 || charlength > 128) {window.prompt("Please start over and select a number between 8-128")
+    var CLinput = (value.charlength);
+  } else { choice2();  
   } }  
-
+  
   function choice2() {
-    var Uppercase = window.prompt("Would you like to use uppercase characters? (Use Y for yes and N for no");
-    if (Uppercase === "Y" || Uppercase==="y" || Uppercase === "yes" || Uppercase || "YES" || "Yes") choice3();
-  } else {
-    return; 
-  }}
+    var confirmUpper = window.confirm("Click ok to use uppercase characters");
+    if (confirmUpper) {
+      var add1 = (Math.floor(Math.random()* Math.floor(uppercase.length)));
+      passwordResult.push(add1)
+      console.log(passwordResult);
+    }
+    
+    choice3();
+  }
 
   function choice3() {
-    var Lowercase = window.prompt("Would you like to use lowercase characters? (Use Y for yes and N for no");
-    if (Lowercase === "Y" || Lowercase==="y" || Lowercase === "yes" || Lowercase || "YES" || "Yes") choice4();
+    var confirmLower = window.confirm("Click ok to use lowercase characters");
+    choice4();
+
   }
 
   function choice4() {
-    var Numbers = window.prompt("Would you like to use number characters? (Use Y for yes and N for no");
-    if (Numbers === "Y" || Numbers==="y" || Numbers === "yes" || Numbers || "YES" || "Yes") choice5(); 
+    var confirmNumbers = window.confirm("Click ok to use number characters")
+    choice5(); 
   }
 
   function choice5() {
-    var Special = window.prompt("Would you like to use special characters? (Use Y for yes and N for no");
-    if (Special === "Y" || Special==="y" || Special === "yes" || Special || "YES" || "Yes") finishedPassword(); 
+    var confirmSpecial = window.confirm("Click ok to use special characters");
+
+
   }
 
 // Write password to the #password input
